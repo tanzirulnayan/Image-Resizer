19fb1fad5e022667ab5e41af3909f5ba27d63fe9
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2019-11-20 09:52:51 +0600

    UI Design WIP

--- Image-Resizer/Form1.Designer.cs
+++ Image-Resizer/Form1.Designer.cs
@@ -39,34 +39,33 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(113, 33);
+            this.label1.Location = new System.Drawing.Point(39, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(73, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Text = "Select Picture";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(116, 69);
+            this.label2.Location = new System.Drawing.Point(44, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(68, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.label2.Text = "Save Picture";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(177, 33);
+            this.textBox1.Location = new System.Drawing.Point(118, 31);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(202, 20);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(209, 69);
+            this.textBox2.Location = new System.Drawing.Point(118, 67);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(202, 20);
             this.textBox2.TabIndex = 3;
             // 
             // button1
@@ -75,16 +74,17 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(337, 65);
+            this.button2.Location = new System.Drawing.Point(326, 65);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "...";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -100,7 +100,6 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Image-Resizer/Form1.cs
+++ Image-Resizer/Form1.cs
@@ -17,12 +17,7 @@ namespace Image_Resizer
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
 
         }
